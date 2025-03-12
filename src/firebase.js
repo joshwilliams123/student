849d1d0e2b899d84742079e9b3360637c8c51579
joshwilliams123 <jaw67@uw.edu>
2025-03-11 23:35:18 -0700

    Initialization of react and firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCG2fRn8Gu0qmR5NFYi-grcnHtrl9zVHrs",
+  authDomain: "harli-platform.firebaseapp.com",
+  projectId: "harli-platform",
+  storageBucket: "harli-platform.firebasestorage.app",
+  messagingSenderId: "380977456320",
+  appId: "1:380977456320:web:8a661984fc53ef0bcff925"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
